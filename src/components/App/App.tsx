0d9e26c7c2e9753d11cbb6350952d48032bd4e29
DzyadzoMM <m.dziadzo@gmail.com>
2025-07-21 01:15:12 +0300

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import CafeInfo from "../CafeInfo/CafeInfo"
+import VoteStats from "../VoteStats/VoteStats"
+import VoteOptions from "../VoteOptions/VoteOptions"
+import css from "./App.module.css"
+
+
+
+export default function App() {
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOptions />
+      </div>
+    </>
+  );
+}
